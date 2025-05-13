0d9d7e7bb02bdcaa8b9afd3aac4d46cb8f6d0d1d
Tanu Shree Gupta <gtanushree534@gmail.com>
2025-05-13 23:55:59 +0530

    feat: add DSA solution in C++

---
+++ Height_of_BT.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int value){
+        val = value;
+        left = right = nullptr;
+    }
+};
+
+class Solution{
+    public:
+    int maxDepth(TreeNode* root){
+        if(root == NULL) return 0;
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
+
+        return 1 + max(lh, rh);
+    }
+};
+
+int main(){
+    // Create sample binary tree
+    //          1
+    //         / \ 
+    //        2   3
+    //           / \ 
+    //          4   6
+    //         /
+    //        5
+
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->right->left = new TreeNode(4);
+    root->right->right = new TreeNode(6);
+    root->right->left->left = new TreeNode(5);
+
+    Solution s;
+
+    int depth = s.maxDepth(root);
+    cout << "Maximum depth of the binary tree is: " << depth << endl;
+
+    return 0;
+}
