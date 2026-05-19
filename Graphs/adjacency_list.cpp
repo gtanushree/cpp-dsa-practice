625abe310a7604c2d224a3d82b8aa50778819415
Tanu Shree Gupta <gtanushree534@gmail.com>
2026-05-20 00:18:10 +0530

    feat: add adjacency list

---
+++ Graphs/adjacency_list.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    // adjacency list for undirected graph
+    // time complexity: O(2E)
+    vector<int> adj[n + 1];
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+}
+
+
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    // adjacency list for directed graph
+    // time complexity: O(E)
+    vector<int> adj[n+1];
+    for(int i = 0; i < m; i++)
+    {
+        int u, v;
+        // u —> v
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+    return 0;
+}
