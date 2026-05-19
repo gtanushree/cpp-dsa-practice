4414d09a6fa771ec5c37475c8322f90fd4f70abc
Tanu Shree Gupta <gtanushree534@gmail.com>
2026-05-20 03:19:59 +0530

    feat: update solution

--- Graphs/Number_of_Provinces.cpp
+++ Graphs/Number_of_Provinces.cpp
@@ -1,42 +1,137 @@
 // Problem Statement: Given an undirected graph with V vertices. Two vertices u and v belong to a single province if there is a path from u to v or v to u. Find the number of provinces. The graph is given as an n x n matrix adj where adj[i][j] = 1 if the ith city and the jth city are directly connected, and adj[i][j] = 0 otherwise.
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
+// Solution class to hold the main logic
 class Solution {
-public:
-    void dfs(int node, vector<vector<int>>& adj, vector<int>& visited) {
+private:
+    // Function to perform DFS traversal
+    void dfs(int node, vector<int> adjList[], int visited[]) {
+        // Mark current node as visited
         visited[node] = 1;
 
-        for(int j = 0; j < adj.size(); j++) {
-            if(adj[node][j] == 1 && !visited[j]) {
-                dfs(j, adj, visited);
+        // Visit all adjacent unvisited nodes
+        for (auto neighbor : adjList[node]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, adjList, visited);
             }
         }
     }
 
-    int numProvinces(vector<vector<int>>& adj) {
-        int V = adj.size();
-        vector<int> visited(V, 0);
-        int count = 0;
+public:
+    // Function to count number of connected components
+    int numProvinces(vector<vector<int>> adj, int V) {
+        // Create adjacency list from adjacency matrix
+        vector<int> adjList[V];
 
-        for(int i = 0; i < V; i++){
-            if(!visited[i]){
-                count++;
-                dfs(i, adj, visited);
+        // Convert matrix to list
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                // Ignore self-loops
+                if (adj[i][j] == 1 && i != j) {
+                    adjList[i].push_back(j);
+                    adjList[j].push_back(i);
+                }
             }
         }
 
+        // Array to keep track of visited nodes
+        int visited[V] = {0};
+
+        // Count of connected components
+        int count = 0;
+
+        // Traverse all nodes
+        for (int i = 0; i < V; i++) {
+            // If node is not visited
+            if (!visited[i]) {
+                // Perform DFS and increment count
+                count++;
+                dfs(i, adjList, visited);
+            }
+        }
+
+        // Return final count
         return count;
     }
 };
 
-int main(){
-    //int adj[4][4] = { {1, 0, 0, 1}, {0, 1, 1, 0}, {0, 1, 1, 0}, {1, 0, 0, 1} };
-    vector<vector<int>> adj = { {1, 0, 0, 1}, {0, 1, 1, 0}, {0, 1, 1, 0}, {1, 0, 0, 1} };
-    Solution s;
-    int result = s.numProvinces(adj);
-    cout << result << endl;
+// Main function to drive the program
+int main() {
+    // Adjacency matrix input
+    vector<vector<int>> adj = {
+        {1, 0, 1},
+        {0, 1, 0},
+        {1, 0, 1}
+    };
+
+    // Number of vertices
+    int V = 3;
+
+    // Create object of Solution
+    Solution obj;
+
+    // Call the function and print result
+    cout << obj.numProvinces(adj, V) << endl;
+
     return 0;
 }
+
+/*
+for leetcode problem: https://leetcode.com/problems/number-of-provinces/#:~:text=A%20province%20is%20a%20group,the%20total%20number%20of%20provinces.
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    // DFS function
+    void dfs(int node, vector<vector<int>>& isConnected, vector<int>& visited) {
+        visited[node] = 1;
+
+        for(int j = 0; j < isConnected.size(); j++) {
+            // Check connection and unvisited node
+            if(isConnected[node][j] == 1 && !visited[j]) {
+                dfs(j, isConnected, visited);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+
+        int n = isConnected.size();
+
+        vector<int> visited(n, 0);
+
+        int provinces = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            // New province found
+            if(!visited[i]) {
+                provinces++;
+                dfs(i, isConnected, visited);
+            }
+        }
+
+        return provinces;
+    }
+};
+
+int main() {
+
+    vector<vector<int>> isConnected = {
+        {1,1,0},
+        {1,1,0},
+        {0,0,1}
+    };
+
+    Solution s;
+
+    cout << "Number of Provinces: "
+         << s.findCircleNum(isConnected);
+
+    return 0;
+}
+*/
